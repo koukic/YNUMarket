262f18af0232778e30b67970b7fac56bfcb060be
EC2 Default User <ec2-user@ip-172-31-89-169.ec2.internal>
2019-10-21 16:06:01 +0000

    Correct the js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,18 +16,3 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
-
- 
-function toggleNav() {
-  var body = document.body;
-  var hamburger = document.getElementById('js-hamburger');
-  var blackBg = document.getElementById('js-black-bg');
-
-  hamburger.addEventListener('click', function() {
-    body.classList.toggle('nav-open');
-  });
-  blackBg.addEventListener('click', function() {
-    body.classList.remove('nav-open');
-  });
-}
-toggleNav();
