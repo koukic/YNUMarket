db3e76521d670293dbbf25300498fb89ece00770
EC2 Default User <ec2-user@ip-172-31-89-169.ec2.internal>
2019-10-23 11:09:39 +0000

    Add placeholder

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -4,11 +4,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  if Rails.env.production?
-     include Cloudinary::CarrierWave
-   else
-     storage :file
- end
+  storage :file
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
