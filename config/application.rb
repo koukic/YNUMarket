6dd0da878bafa7aba995866f2efbfc0b5b78ba8c
中條航紀 <ck227200@gmail.com>
2019-11-19 21:50:40 +0900

    Add carrierwaveset

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,6 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-config.assets.initialize_on_precompile = true
 
 module Testinformation
   class Application < Rails::Application
--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,5 +1,6 @@
 if Rails.env.production?
   CarrierWave.configure do |config|
+    config.fog_provider = 'fog/aws'
     config.fog_credentials = {
       # Amazon S3用の設定
       :provider              => 'AWS',
