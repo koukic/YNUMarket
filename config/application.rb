a514396f5a3b2888a7ae651cf771be2eecdecb2d
中條航紀 <ck227200@gmail.com>
2019-11-19 21:33:44 +0900

    Add pipeline

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,8 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+config.assets.initialize_on_precompile = false
+
 module Testinformation
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
