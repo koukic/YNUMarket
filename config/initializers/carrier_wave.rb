4ea616363e525b0db133303909202954918ec0d1
中條航紀 <ck227200@gmail.com>
2019-11-25 13:54:21 +0900

    dlete host

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -6,7 +6,6 @@ if Rails.env.production?
   CarrierWave.configure do |config|
     config.storage :fog
     config.fog_provider = 'fog/aws'
-    config.asset_host = 'https://testinformation.1.s3.amazonaws.com'
     config.fog_public = false
     config.fog_credentials = {
       # Amazon S3用の設定
