a3c0c3764fd006034d9e2aab5f4cbca3d692c685
中條航紀 <ck227200@gmail.com>
2019-12-08 22:57:44 +0900

    :+1:Change host

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -15,5 +15,6 @@ if Rails.env.production?
       :aws_secret_access_key => ENV['S3_SECRET_KEY'],
     }
     config.fog_directory     =  ENV['S3_BUCKET']
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/testinformation.1'
   end
 end
