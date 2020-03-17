e2c6b9531d9334bf032c9dba8f3b5ced578ea307
中條航紀 <ck227200@gmail.com>
2020-03-17 16:19:09 +0900

    :+1:Add id

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,11 +1,5 @@
 class RoomsController < ApplicationController
   before_action :authenticate_user!
-  def create
-    @room = Room.create
-    @entry1 = Entry.create(room_id: @room.id, user_id: current_user.id)
-    @entry2 = Entry.create(params.require(:entry).permit(:user_id, :room_id).merge(room_id: @room.id))
-    redirect_to "/rooms/#{@room.id}"
-  end
 
   def show
     @room = Room.find(params[:id])
@@ -16,5 +10,12 @@ class RoomsController < ApplicationController
     else
       redirect_back(fallback_location: root_path)
     end
+  end
+
+  def create
+    @room = Room.create
+    @entry1 = Entry.create(room_id: @room.id, user_id: current_user.id)
+    @entry2 = Entry.create(params.require(:entry).permit(:user_id, :room_id).merge(room_id: @room.id))
+    redirect_to "/rooms/#{@room.id}"
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,5 @@ class User < ApplicationRecord
   has_many :reviews,dependent: :destroy
   has_many :messages, dependent: :destroy
   has_many :entries, dependent: :destroy
+  has_many :rooms, through: :entries
 end
