1a30075a2aeeb8f12bbe02ea2a0249caa348e7fa
中條航紀 <ck227200@gmail.com>
2020-03-20 16:04:39 +0900

    :+1:Add message

--- app/channels/room_channel.rb
+++ app/channels/room_channel.rb
@@ -1,8 +1,8 @@
 class RoomChannel < ApplicationCable::Channel
   def subscribed
     # stream_from "some_channel"
-    # stream_from "room_channel_#{params['room']}"
-    stream_from "room_channel"
+    stream_from "room_channel_#{params['room']}"
+    # stream_from "room_channel"
   end
 
   def unsubscribed
@@ -10,7 +10,7 @@ class RoomChannel < ApplicationCable::Channel
   end
 
   def speak(data)
-     # Message.create! message: data['message'], user_id: current_user.id, room_id: params['room']
-     ActionCable.server.broadcast 'room_channel', message: data['message']
+     Message.create! message: data['message'], user_id: current_user.id, room_id: params['room']
+     # ActionCable.server.broadcast 'room_channel', message: data['message']
   end
 end
--- app/jobs/message_broadcast_job.rb
+++ app/jobs/message_broadcast_job.rb
@@ -2,7 +2,6 @@ class MessageBroadcastJob < ApplicationJob
   queue_as :default
 
   def perform(message)
-    binding.pry
     ActionCable.server.broadcast "room_channel_#{message.room_id}", message: render_message(message)
   end
 
