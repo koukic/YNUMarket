d51f2df38260044f7d38364eb9828c6b9ee4532c
中條航紀 <ck227200@gmail.com>
2020-03-22 11:16:54 +0900

    :+1:Separate create

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,14 @@
 class ApplicationController < ActionController::Base
   # private
   # def after_sign_in_path_for(resource)
-  #   if session[:cart_id].present?
-  #     @cart = Cart.find(session[:cart_id])
-  #     root_path
-  #   end
+  #   @cart = Cart.find(session[:cart_id])
+  # rescue ActiveRecord::RecordNotFound
+  #   @cart = Cart.create
+  #   session[:cart_id] = @cart.id
+  # end
+  # def after_sign_out_path_for(resource)
+  #   @cart = Cart.create
+  #   session[:cart_id] = @cart.id
+  #   root_path
   # end
 end
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,8 +3,10 @@ class MessagesController < ApplicationController
 
    def create
      if Entry.where(user_id: current_user.id, room_id: params[:message][:room_id]).present?
-       @message = Message.create(params.require(:message).permit(:user_id, :content, :room_id).merge(user_id: current_user.id, current_user: current_user))
+       @message = Message.new(params.require(:message).permit(:user_id, :content, :room_id).merge(user_id: current_user.id))
        @message.current_user = current_user
+       @message.save
+       pp @message
        redirect_to "/rooms/#{@message.room_id}"
      else
        redirect_back(fallback_location: root_path)
