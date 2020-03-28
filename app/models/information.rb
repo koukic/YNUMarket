0d4a6f0274f946639e297ba4e68c5caed15651a0
中條航紀 <ck227200@gmail.com>
2020-03-28 22:41:02 +0900

    :+1:Add array

--- app/models/information.rb
+++ app/models/information.rb
@@ -14,7 +14,7 @@ class Information < ApplicationRecord
   allowed."}
   validates :title, length: { maximum: 140, too_long: "%{count} characters is the maximum
   allowed."}
-  CONDITION = {["new"] => 'new', ["fair"] => 'fair', ['サークル', 'サークル・バスケ'] => 'サークル・バスケ' }
+  CONDITION = {'new' => ["new"],'fair' => ["fair"],'サークル・バスケ' => ['サークル', 'サークル・バスケ']}
 
   class << self
     def search(query)
