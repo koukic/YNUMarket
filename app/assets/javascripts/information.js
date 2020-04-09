149119bef849eab1988ed35549ccf901d2abf454
中條航紀 <ck227200@gmail.com>
2020-04-09 21:03:24 +0900

    :+1:Delete unneccesary

--- app/assets/javascripts/information.js
+++ app/assets/javascripts/information.js
@@ -1,29 +1,3 @@
-$(function(){
-    $('#information_image').change(function(){
-        $('img').remove();
-        var file = $(this).prop('files');
-        $(file).each(function(i) {
-        if(!file[i].type.match('image.*')){
-            return;
-        }
-        var fileReader = new FileReader();
-        fileReader.onloadend = function() {
-            $('#result').append('<img src="' + fileReader.result + '"/>');
-        }
-        fileReader.readAsDataURL(file[i]);
-        });
-    });
-});
-
-$(document).on('turbolinks:load', function(){
-  $('.dropdown').on('click', function() {
-    $('.dropdown').toggleClass('is-active');
-  });
-  $('.dropdown-item a').on('click', function() {
-    $('.dropdown').toggleClass('is-active');
-  });
-})
-
 var content = document.getElementById("dropdown-menu2");
 var myDIV = document.getElementById("myDIV");
 var tabContents = document.getElementsByClassName("tab-content");
