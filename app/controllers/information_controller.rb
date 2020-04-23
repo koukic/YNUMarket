38e08e1c92db3d0507db05eec939e056f7692d5d
中條航紀 <ck227200@gmail.com>
2020-04-23 22:41:44 +0900

    :+1:Add user

--- app/controllers/information_controller.rb
+++ app/controllers/information_controller.rb
@@ -1,35 +1,39 @@
+# frozen_string_literal: true
+
 class InformationController < ApplicationController
   before_action :set_information, only: %i[show edit update destroy]
-  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!, except: %i[index show]
   before_action :set_room, only: %i[index show]
   # GET /information
   # GET /information.json
   def index
-    # @user = User.find(1)
+    @user = User.find(1)
     if params[:tag_name]
-      @informations = Information.tagged_with("#{params[:tag_name]}").order(:id).page params[:page]
+      @informations = Information.tagged_with(params[:tag_name].to_s).order(:id).page params[:page]
     else
       @informations = Information.order(created_at: :desc).page params[:page]
     end
 
-    @information_furima = Information.tagged_with("フリマ").order(created_at: :desc).page params[:page]
-    @information_service = Information.tagged_with("サービス").order(created_at: :desc).page params[:page]
-    @information_circle = Information.tagged_with("サークル").order(:id).page params[:page]
+    @information_furima = Information.tagged_with('フリマ').order(created_at: :desc).page params[:page]
+    @information_service = Information.tagged_with('サービス').order(created_at: :desc).page params[:page]
+    @information_circle = Information.tagged_with('サークル').order(:id).page params[:page]
   end
+
   # GET /information/1
   # GET /information/1.json
   def show
     @information = Information.find(params[:id])
   end
+
   # GET /information/new
   def new
     @information = current_user.informations.build
     @information.information_images.build
   end
-  # GET /information/1/edit
-  def edit
 
-  end
+  # GET /information/1/edit
+  def edit; end
+
   # POST /information
   # POST /information.json
   def create
@@ -41,15 +45,15 @@ class InformationController < ApplicationController
     end
 
     if @information.tag_list == ['フリマand教科書']
-      @information.tag_list = ['フリマ','教科書']
+      @information.tag_list = %W[\u30D5\u30EA\u30DE \u6559\u79D1\u66F8]
     elsif @information.tag_list == ['フリマand本・漫画']
       @information.tag_list = ['フリマ', '本・漫画']
     elsif @information.tag_list == ['フリマandインテリア・家具']
       @information.tag_list == ['フリマ', 'インテリア・家具']
     elsif @information.tag_list == ['フリマandスポーツ']
-      @information.tag_list = ['フリマ', 'スポーツ']
+      @information.tag_list = %W[\u30D5\u30EA\u30DE \u30B9\u30DD\u30FC\u30C4]
     elsif @information.tag_list == ['フリマandその他']
-      @information.tag_list = ['フリマ', 'その他']
+      @information.tag_list = %W[\u30D5\u30EA\u30DE \u305D\u306E\u4ED6]
     elsif @information.tag_list == ['サービス']
       @information.tag_list = ['サービス']
     elsif @information.tag_list == ['サークルandサークル・スポーツ']
@@ -68,6 +72,7 @@ class InformationController < ApplicationController
       end
     end
   end
+
   # PATCH/PUT /information/1
   # PATCH/PUT /information/1.json
   def update
@@ -81,6 +86,7 @@ class InformationController < ApplicationController
       end
     end
   end
+
   # DELETE /information/1
   # DELETE /information/1.json
   def destroy
@@ -90,26 +96,29 @@ class InformationController < ApplicationController
       format.json { head :no_content }
     end
   end
+
   def search
     @informations = Information.search(params[:q]).page(params[:page])
-    render "index"
+    render 'index'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_information
-      @information = Information.find(params[:id])
-    end
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def information_params
-      params.require(:information).permit(:condition, :title, :description, :tag_list, :price, images: [])
-    end
 
-    def set_room
-      @cart = Cart.find_by(id: current_user&.cart&.id)
-      @rooms = current_user&.rooms
-      @chat_rooms = @rooms&.select do |room|
-        User.find_by(id: room.entries.first.user_id).name != current_user.name
-      end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_information
+    @information = Information.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def information_params
+    params.require(:information).permit(:condition, :title, :description, :tag_list, :price, images: [])
+  end
+
+  def set_room
+    @cart = Cart.find_by(id: current_user&.cart&.id)
+    @rooms = current_user&.rooms
+    @chat_rooms = @rooms&.select do |room|
+      User.find_by(id: room.entries.first.user_id).name != current_user.name
     end
+  end
 end
